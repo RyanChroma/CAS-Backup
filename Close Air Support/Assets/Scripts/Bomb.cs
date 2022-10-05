8d8359858b2aee0931ac7bfd0ec370f897d20a59
RyanCua <ttff2ryan@gmail.com>
2022-10-06 04:56:44 +0700

    Bomb detect floor

--- Close Air Support/Assets/Scripts/Bomb.cs
+++ Close Air Support/Assets/Scripts/Bomb.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class Bomb : MonoBehaviour
 {
     public float bombDestroyTimer = 0.0f;
+    public BoxCollider bombCollider;
+    public Transform bombExplosionEffect;
 
-    public void OnTriggerEnter(Collider other)
+    public void OnTriggerEnter(Collider collision)
     {
-        
+        Debug.Log("Bomb hit");
+        Destroy(gameObject);
     }
 }
